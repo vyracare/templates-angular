9e4e5eb6b6dd4699d0933b54ad99da02b83375ef
Lênin Müller Sampaio <lenin_muller@uol.com.br>
2026-02-23 22:29:51 -0300

    feat: configuração jest

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,12 +1,10 @@
-import { provideZonelessChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
-      providers: [provideZonelessChangeDetection()]
+      imports: [App]
     }).compileComponents();
   });
 
@@ -20,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Dashboard Standalone MFE');
+    expect(compiled.querySelector('h1')?.textContent).toContain('[name-generic]');
   });
 });
